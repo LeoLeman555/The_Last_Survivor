760a87bb1f92a9dde5a61ebfd226c7e9e01034d9
Léo Leman <leo.leman555@gmail.com>
2024-12-04 16:15:01 +0100

    v0.47.9 feat(build): add cx_Freeze setup for .exe creation

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+setup(
+  name="The Last Survivor",
+  version="0.5",
+  options={
+    "build_exe": {
+      "packages": ["pygame", "pyscroll", "pytmx"],
+      "include_files": ["res/", "src/", "data/", "LICENSE", "README.md", "requirements.txt"],
+    }
+  },
+  executables=[
+    Executable(
+      "main.py",
+      target_name="The Last Survivor.exe",
+      base="Win32GUI",
+    )
+  ],
+)
